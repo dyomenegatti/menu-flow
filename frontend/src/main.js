5af4c2584832a52064f6e7d7324a4c4bc93e316c
Dyo <dyo.menegatti@hotmail.com>
2026-06-23 16:02:54 -0300

    feat: instala dependencias e corrige arquivos principais

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'vuetify/styles'
-import './style.css'
-import '@mdi/font/css/materialdesignicons.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import 'vuetify/styles';
+import './style.css';
+import '@mdi/font/css/materialdesignicons.css';
 import vuetify from './app/providers/vuetify/vuetify';
+
 import router from './app/router/index';
+
+import { createPinia } from 'pinia';
+const pinia = createPinia();
 
 createApp(App)
     .use(vuetify)
     .use(router)
+    .use(pinia)
     .mount('#app')
